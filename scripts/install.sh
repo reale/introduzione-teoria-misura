9e3e9e7064eaec3b0cd68f297c09d7b965408592
Roberto Reale <roberto.reale@linux.com>
2017-09-28 15:59:48 +0200

    Reformat install code

---
+++ scripts/install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo apt-get -qq update && sudo apt-get install -y --no-install-recommends texlive-fonts-recommended texlive-latex-extra texlive-fonts-extra texlive-latex-recommended texlive-lang-italian texlive-lang-cyrillic cm-super latexmk
